561ea0cc63e7369e395eef50b42619f6375edb88
massaru <Davi-Massaru>
2021-03-09 19:14:36 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'App/app.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Jokenpo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'じゃんけんぽん!'),
+    );
+  }
+}
